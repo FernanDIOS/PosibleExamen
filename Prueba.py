54f7cc43475200b100cc5e3bcf1c1e7586244944
Fernando Sastre Pérez <2021289@iesluisbraille.es>
2022-03-09 08:48:47 +0100

    Primero

---
+++ Prueba.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 17:27:05 2022
+
+@author: USR
+"""
+
+Nombre = input("¿Como te llamas? -> ")
+Edad = int(input("¿Que edad tienes? -> "))
+print(f"Tu nombre es {Nombre} y tienes {Edad} años")
